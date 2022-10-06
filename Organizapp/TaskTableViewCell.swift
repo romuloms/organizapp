a916ffd2606c66aa3b6fd8f3ac74296afb3544dc
Ádrian Gama <adrian.gama.se@gmail.com>
2022-10-06 14:40:06 +0000

    fix: project resources

---
+++ Organizapp/TaskTableViewCell.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
